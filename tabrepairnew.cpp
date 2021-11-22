657369217ff5deab6ec309bc0a624cd0074ed507
solderer <none@none>
2021-11-23 01:11:26 +0200

    Небольшие изменения формы приёма в ремонт. Нижняя панель инструментов выполнена непосредственно на форме, а не в виде объекта из заранее подготовленного класса, т. к. этот класс требует реализации отдельных процедур для возможности добавления на нижнюю панель элементов; в целом большой разницы как набрасывать элементы нижней панели нет; однако, это не окончательное решение.

--- tabrepairnew.cpp
+++ tabrepairnew.cpp
@@ -1,11 +1,14 @@
 #include "tabrepairnew.h"
 #include "ui_tabrepairnew.h"
+#include "bottoolbarwidget.h"
 
 tabRepairNew::tabRepairNew(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::tabRepairNew)
 {
     ui->setupUi(this);
+//    BotToolbarWidget* botToolbarWidget_ = new BotToolbarWidget(ui->botToolbar);
+//    ui->gridLayout->addWidget(botToolbarWidget_, 30, 0, 1, 5, Qt::AlignBottom);
 }
 
 tabRepairNew::~tabRepairNew()
